2d71d3a1be0cddfdaf7f551f8e10cf0475468487
heyyrishabhhh44 <heyyrishabh44@gmail.com>
2026-01-26 12:46:06 +0530

    Added real-time screen usage tracking logic

--- tracker.py
+++ tracker.py
@@ -1,1 +1,16 @@
+import time
+
 print("Screen Time Monitor Started")
+
+start_time = time.time()
+
+try:
+    while True:
+        time.sleep(5)
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+        minutes = elapsed_time / 60
+        print(f"Screen Time: {round(minutes, 2)} minutes")
+
+except KeyboardInterrupt:
+    print("\nMonitoring Stopped")
