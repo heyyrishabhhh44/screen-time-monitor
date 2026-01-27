5f7d5049a191cea2af19554595d4729ccbf5de52
heyyrishabhhh44 <heyyrishabh44@gmail.com>
2026-01-27 23:22:18 +0530

    Improve user-friendly output messages
    Added better console instructions and formatted output display

--- tracker.py
+++ tracker.py
@@ -1,6 +1,7 @@
 import time
 
-print("Screen Time Monitor Started")
+print("Screen Time Monitor Started...")
+print("Press CTRL + C to stop tracking")
 
 start_time = time.time()
 
@@ -10,7 +11,8 @@ try:
         current_time = time.time()
         elapsed_time = current_time - start_time
         minutes = elapsed_time / 60
-        print(f"Screen Time: {round(minutes, 2)} minutes")
+        print(f"Active Screen Time: {round(minutes, 2)} minutes")
+
 
 except KeyboardInterrupt:
     print("\nMonitoring Stopped")
